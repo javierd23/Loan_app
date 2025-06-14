05e95576f6984e413a5d6552c4daf4f7711f24ad
carlosenriquejavierdelarosa <carlosjjavier05@gmail.com>
2025-06-14 13:36:21 -0400

    Update

--- home/views.py
+++ home/views.py
@@ -1,10 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-
-def index(request):
+def home(request):
     return HttpResponse("Hello, world. You are seening the first app t"
                         "hat will calculte your loan for you.")
-
-
-
--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'loan.apps.LoanConfig',
+    'home.apps.HomeConfig',
 ]
 
 MIDDLEWARE = [
