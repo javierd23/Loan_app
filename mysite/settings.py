f1481c1aafc9e448bf66ee5d931a65da4a69cea2
carlosenriquejavierdelarosa <carlosjjavier05@gmail.com>
2025-08-14 23:27:15 -0400

    On this link, I added some links to the apps to maintain the user engaged and not to lose them while navigating. I did the collectstatic today and the build for the tailwind, I also fixed some dark mode not config.

--- mysite/settings.py
+++ mysite/settings.py
@@ -182,7 +182,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'home', 'static'),
+]
+
+
 #Root of the upload media for the user, this is for profile photo
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  #storing the upload file by users...
 MEDIA_URL = '/media/'
