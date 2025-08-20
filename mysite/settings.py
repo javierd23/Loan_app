56dd57d5045cbf68d2bdc8c1b1f02b128b527278
carlosenriquejavierdelarosa <carlosjjavier05@gmail.com>
2025-08-19 23:49:56 -0400

    Updating the django/config

--- mysite/settings.py
+++ mysite/settings.py
@@ -115,7 +115,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "django_browser_reload.middleware.BrowserReloadMiddleware",
+    #"django_browser_reload.middleware.BrowserReloadMiddleware",
 
 ]
 
