cae317d70f2f11b9e85156343728bfa57931d354
carlosenriquejavierdelarosa <carlosjjavier05@gmail.com>
2025-08-20 12:30:00 -0400

    Adding a view for the https

--- home/views.py
+++ home/views.py
@@ -43,3 +43,9 @@ def my_custom_page_not_found_view(request, exception):
 
 def my_custom_error_view(request):
     return render(request, "500.html", {}, status=500)
+
+
+from django.http import HttpResponse
+
+def health_check(request):
+    return HttpResponse("OK")
