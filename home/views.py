34bde1d82e57b2b4111c9cfe83957883251d1208
carlosenriquejavierdelarosa <carlosjjavier05@gmail.com>
2025-08-22 00:16:47 -0400

    Fixing 404 html and 500 view

--- home/views.py
+++ home/views.py
@@ -39,8 +39,8 @@ class FeedbackCreateView(CreateView):
 from django.shortcuts import render
 
 def my_custom_page_not_found_view(request, exception):
-    return render(request, "404.html", {}, status=404)
+    return render(request, "home/404.html", {}, status=404)
 
 def my_custom_error_view(request):
-    return render(request, "500.html", {}, status=500)
+    return render(request, "home/500.html", {}, status=500)
 
