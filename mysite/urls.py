f1a0d662ef717fc5c3350c90a2281cb1796aa2e9
carlos23 <carlosjjavier05@gmail.com>
2025-08-23 13:45:34 +0000

    fixing media url and path to display media in productions

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,6 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
@@ -32,6 +35,10 @@ urlpatterns = [
 
 
 ]
+
+#Media
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # 404 http views...
 handler404 = my_custom_page_not_found_view
