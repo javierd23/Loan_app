a5381283e2fb64c03fc24a12cd0a42a9f5d79c6d
carlosenriquejavierdelarosa <carlosjjavier05@gmail.com>
2025-08-28 07:35:18 -0400

    Removing some unnecesarry url from url main project.

--- mysite/urls.py
+++ mysite/urls.py
@@ -31,14 +31,9 @@ urlpatterns = [
     path("api_prv/forum/", include("forum.api.urls")),
     path("accounts/", include("accounts.urls")),
     path('accounts/', include('django.contrib.auth.urls')),
-    path("__reload__/", include("django_browser_reload.urls")),
-
 
 ]
 
-#Media
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # 404 http views...
 handler404 = my_custom_page_not_found_view
